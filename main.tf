6c54fc510f04bcee5fd4ef150830d2c7c41f8c39
Adodoadjie <eadodoadjie@yahoo.com>
2023-05-14 23:46:47 +0000

    Created a terraform module for provisioning instance

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:177222388ecd5af47e4321eaf332baad9f22fc502da3a6afbb98ca4b585fc649
-size 362
+oid sha256:5ea35b840f2c907aaf6de41f5017ad66e3d33fa1651b3a40de894416bbc0a7f8
+size 385
